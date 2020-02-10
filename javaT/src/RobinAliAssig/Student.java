d910c6b5d4b5184e40cd8f5f77dc9e76f2039878
Sanjeev Kumar Saini <sanjeevsaini11235@gmail.com>
2020-02-10 13:16:51 +0530

    p12

---
+++ javaT/src/RobinAliAssig/Student.java
@@ -0,0 +1,24 @@
+package RobinAliAssig;
+
+public class Student {
+
+	final String name;
+	final int enroll;
+	
+	public Student(String name, int enroll) {
+		super();
+		this.name = name;
+		this.enroll = enroll;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getEnroll() {
+		return enroll;
+	}
+	
+	
+	
+}
---
+++ javaT/src/RobinAliAssig/StudentMain.java
@@ -0,0 +1,12 @@
+package RobinAliAssig;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		Student s= new Student("AA", 101);
+		System.out.println(s.getName() +" "+ s.getEnroll());
+		
+//		s.enroll = 100;
+		
+	}
+}
