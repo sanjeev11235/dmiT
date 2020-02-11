fbb82a27f1e5ceffaef0f9e2c3747cffccf451f9
Sanjeev Kumar Saini <sanjeevsaini11235@gmail.com>
2020-02-11 18:26:49 +0530

    p16

--- javaT/src/lamdaEx/ThreadMainLambda.java
+++ javaT/src/lamdaEx/ThreadMainLambda.java
@@ -6,9 +6,8 @@ public class ThreadMainLambda {
 
 	public static void main(String[] args) {
 		
-		 
+		//Thread without lambda
 		Runnable r1 = new Runnable() {
-			
 			@Override
 			public void run() {
 				System.out.println("In Runnable r1");
@@ -17,12 +16,9 @@ public class ThreadMainLambda {
 		Thread t1= new Thread(r1);
 		t1.start();
 		
-		Runnable r2 = new Runnable() {
-			
-			@Override
-			public void run() {
+		//Thread using lambda
+		Runnable r2 = ()-> {
 				System.out.println("In Runnable r2");
-			}
 		};
 		Thread t2 = new Thread(r2);
 		t2.start();
